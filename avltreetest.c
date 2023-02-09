cacf767c015ccf9f31010892e0e5dfc9122184f3
Alexey Fedorenko <lee_peha@vk.com>
2023-02-09 20:57:11 +0300

    Remove check library. Test only on c

--- avltreetest.c
+++ avltreetest.c
@@ -1,95 +1,65 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <check.h>
 
 #include "avltree.h"
 
 int cmp( void *data0, void *data1 )
 {
-  return *(int *)data0 - *(int *)data1;
+  return *(char *)data0 - *(char *)data1;
 }
 
 void free_data( void *data ) {}
 
-START_TEST (test_insert)
-{
-  int data[10];
-
-  AvlTree tree;
-  AvlTree_init( &tree );
-  int i;
-  for( i = 0; i < 10; i++ ) {
-    data[i] = i;
-    AvlTree_insert( &tree, (void*)&data[i], cmp );
-    ck_assert( *(int *)AvlTree_find( &tree, (void*)&data[i], cmp ) == i );
-  }
-  AvlTree_clean( &tree, free_data );
-}
-END_TEST;
-
-START_TEST (test_delete)
-{
-  int data[10];
-
-  AvlTree tree;
-  AvlTree_init( &tree );
-  int i;
-  for( i = 1; i < 10; i++ ) {
-    data[i] = i;
-    AvlTree_insert( &tree, (void*)&data[i], cmp );
-    ck_assert( *(int *)AvlTree_find( &tree, (void*)&data[i], cmp ) == i );
-    AvlTree_delete( &tree, (void*)&data[i], cmp, free_data );
-    ck_assert( AvlTree_find( &tree, (void*)&data[i], cmp ) == NULL );
-  }
-  AvlTree_clean( &tree, free_data );
-}
-END_TEST;
-
-START_TEST (test_valid_inorder_iteration)
-{
-  int data[10];
-  AvlTree tree;
-  AvlTree_init( &tree );
-  int i;
-  for( i = 0; i < 10; i++ ) {
-    data[i] = i;
-    AvlTree_insert( &tree, (void*)&data[i], cmp );
-  }
-
-  AvlTreeIter it;
-  AvlTree_iter_init( &it, &tree );
-  void *next = AvlTreeIter_next( &it );
-  for(i = 0 ; next != NULL; next = AvlTreeIter_next( &it ), i++ ) {
-    ck_assert( *(int *)next == data[i] );
-  }
-}
-END_TEST;
-
-Suite *common_suite()
-{
-  Suite *s;
-  TCase *tc_common;
-
-  s = suite_create( "Common testing AVL trees." );
-  tc_common = tcase_create( "Inserting" );
-  tcase_add_test( tc_common, test_insert );
-  tcase_add_test( tc_common, test_delete );
-  tcase_add_test( tc_common, test_valid_inorder_iteration );
-  suite_add_tcase( s, tc_common );
-  return s;
-}
-
 int main()
 {
-  int number_failed;
-  Suite *s;
-  SRunner *sr;
+  char table[48] = {
+    'A',
+    'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'R',
+    'S', 'T', 'U', 'V', 'W',
+    'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e',
+    'f', 'g', 'h', 'j', 'k',
+    'l', 'm', 'n', 'o', 'p',
+    'r', 's', 't', 'u', 'v',
+    'w', 'x', 'y', 'z'
+  };
 
-  s = common_suite();
-  sr = srunner_create(s);
+  AvlTree tree;
+  AvlTree_init( &tree );
 
-  srunner_run_all(sr, CK_NORMAL);
-  number_failed = srunner_ntests_failed(sr);
-  srunner_free(sr);
-  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+  int i;
+  for( i = 0; i < 48; i++ ) {
+    AvlTree_insert( &tree, (void *)&(table[i]), cmp );
+    void *item;
+    int j;
+    AvlTreeIter it;
+    AvlTree_iter_init( &it, &tree );
+    for( j = 0, item = AvlTreeIter_next( &it ); item != NULL; j++, item = AvlTreeIter_next( &it ) ) {
+      printf("%c ", *(char *)item);
+      if( *(char *)item != table[j] ) {
+        fprintf( stderr, "Test Failure tree ordering.\n");
+        return EXIT_FAILURE;
+      }
+    }
+    putc('\n', stdout);
+  }
+
+  for( i = 0; i < 48; i++ ) {
+    AvlTree_delete( &tree, &table[i], cmp, free_data );
+  }
+
+  AvlTree tree1;
+  AvlTree_init( &tree1 );
+
+  char table1[8] = { 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a' };
+  for( i = 0; i < 8; i++ ) {
+    AvlTree_insert( &tree1, (void *)&(table1[i]), cmp );
+  }
+
+  char a = 'a';
+  AvlTree_delete( &tree1, (void *)&a, cmp, free_data );
+
+  return EXIT_SUCCESS;
 }
